8b636a534e8afddd0710290265edc41b8df9dc2e
teenee0 <qwerty.gogi@mail.ru>
2024-08-29 19:43:20 +0500

    Сделал все

--- C++_Calculator/C++_Calculator.cpp
+++ C++_Calculator/C++_Calculator.cpp
@@ -2,14 +2,60 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
+template <class T1, class T2>
 class Calculator {
+private:
+    float num1;
+    float num2;
+public:
+
+    Calculator(T1 num1, T2 num2) {
+        this->num1 = static_cast<float>(num1);
+        this->num2 = static_cast<float>(num2);
+
+        cout << "Первая цифра равна " << this->num1 << " вторая цифра равна " << this->num2 << endl;
+
+        
+    }
+
+    void plus() {
+        cout << this->num1 << " + " << this->num2<< " = " << this->num1 + this->num2 << endl;
+        
+    }
+
+    void minus() {
+        cout << this->num1 << " - " << this->num2 << " = " << this->num1 - this->num2 << endl;
+
+    }
+    void multiply() {
+        cout << this->num1 << " * " << this->num2 << " = " << this->num1 * this->num2 << endl;
+
+    }
+    void divide() {
+        if (this->num2 == 0) {
+            cout << "На ноль делить нельзя!";
+            return;
+            
+        }
+        cout << this->num1 << " : " << this->num2 << " = " << this->num1 / this->num2 << endl;
+    }
+    
+
 
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "RU");
+    Calculator<int, int> calc(1, 2);
+    calc.plus();
+    calc.minus();
+    calc.multiply();
+    calc.divide();
+    Calculator<int, int> calc2(45, 0);
+    calc2.divide();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
